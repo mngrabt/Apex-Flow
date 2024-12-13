5d05dcaf6cdb1e2163cf727f3562053f9c3229df
mngrabt <167529240+mngrabt@users.noreply.github.com>
2024-12-13 20:36:09 +0500

    PORT issue fix

--- vite.config.ts
+++ vite.config.ts
@@ -13,4 +13,9 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  preview: {
+    port: Number(process.env.PORT) || 4173,
+    host: '0.0.0.0',
+    strictPort: true,
+  },
 });
