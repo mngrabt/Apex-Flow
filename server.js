5843f6e5657e1ec2609e73be0adf26bbb8a55aec
mngrabt <167529240+mngrabt@users.noreply.github.com>
2024-12-17 10:38:30 +0500

    Apex Flow 1.3.2
    - Launch error fix

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const __dirname = dirname(__filename);
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.BOT_SERVER_PORT || 3001;
 
 // Middleware
 app.use(express.json());
